d8456ca14e96af630e3a2d2af4abb10ba3e5fdd3
DDL <dimin21590@gmail.com>
2021-11-17 19:01:11 +0200

    PIO free running mode implemented

--- src/Multislope/build/CMakeFiles/3.18.4/CMakeSystem.cmake
+++ src/Multislope/build/CMakeFiles/3.18.4/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-5.11.0-38-generic")
+set(CMAKE_HOST_SYSTEM "Linux-5.11.0-40-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "5.11.0-38-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "5.11.0-40-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/dima/rpico/pico/pico-sdk/cmake/preload/toolchains/pico_arm_gcc.cmake")
--- src/Multislope/main.c
+++ src/Multislope/main.c
@@ -1,113 +1,45 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pwm.h"
-#include "hardware/irq.h"
-
-volatile uint multiSlopeFreq = 200000; // clock frequency for multislope
-volatile uint duty_cycle = 50;
-uint nsec_period = 0;
-uint wrap_num = 0;
-uint duty_val = 0;
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "ms.pio.h"
 
 const uint8_t MDAT = 17; // SPI MISO Pin
 const uint8_t MCLK = 18; // SPI Clock Pin
 const uint8_t M_EN = 16;
 
-const u_int8_t MUX_A0 = 22;
-const u_int8_t MUX_A1 = 26;
-const u_int8_t MUX_A2 = 27;
+const uint8_t MUX_A0 = 22;
+const uint8_t MUX_A1 = 26;
+const uint8_t MUX_A2 = 27;
 
-const u_int8_t PWMA = 20; // need to be in the same PWM slice
-const u_int8_t PWMB = 21;
-const u_int8_t COMP = 19;
 
-void updatePWMVals(){
-    nsec_period = 1000000000/multiSlopeFreq; // compute the period in nanoseconds
-    wrap_num = nsec_period/8; // (Optimize, by getting the actual frequency and using that for math) since the CPU frequency is 125MHz - one iteration of the PWM counter is 8 ns
-    duty_val = duty_cycle * wrap_num / 100; // simple percentage proportion
-}
-
-void on_pwm_wrap() {
-    if(gpio_get_out_level(COMP)){
-        duty_cycle = 80;
-    }else{
-        duty_cycle = 20;
-    }
-    updatePWMVals();
-    pwm_set_gpio_level(PWMA, duty_val);
-    pwm_set_gpio_level(PWMB, duty_val);
-}
+const uint8_t PWMA = 20;//24; // need to be in the same PWM slice
+const uint8_t PWMB = 21;//25;
+const uint8_t COMP = 19;
 
 int main() {
-    stdio_init_all();
-    updatePWMVals();
+    static const float pio_freq = 20000000; // 20Mhz - 100 slices in a 100KHz square wave
 
+    // Choose PIO instance (0 or 1)
+    PIO pio = pio0;
 
-    gpio_set_function(PWMA, GPIO_FUNC_PWM); // set to PWM mode
-    gpio_set_function(PWMB, GPIO_FUNC_PWM);
+    // Get first free state machine in PIO 0
+    uint sm = pio_claim_unused_sm(pio, true);
 
-    uint pwm1_slice_num = pwm_gpio_to_slice_num(PWMA); // PWM is divided in to "slices" this function returns the slice number form GPIO pin value
+    // Add PIO program to PIO instruction memory. SDK will find location and
+    // return with the memory offset of the program.
+    uint offset = pio_add_program(pio, &ms_program);
 
-    // Mask our slice's IRQ output into the PWM block's single interrupt line,
-    // and register our interrupt handler
-    pwm_clear_irq(pwm1_slice_num);
-    pwm_set_irq_enabled(pwm1_slice_num, true);
-    irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
-    irq_set_enabled(PWM_IRQ_WRAP, true);
+    // Calculate the PIO clock divider
+    float div = (float)clock_get_hz(clk_sys) / pio_freq;
 
+    // Initialize the program using the helper function in our .pio file
+    ms_program_init(pio, sm, offset, PWMA, COMP, div);
 
-
-    pwm_set_wrap(pwm1_slice_num, wrap_num); //only need to set one of them if several PWM pins are in the same slice
-
-    pwm_set_gpio_level(PWMA, duty_val); // this is how you set duty cycle
-
-    pwm_set_output_polarity(pwm1_slice_num, false, true); // if we want to invert pins on the slice set to true
-
-    pwm_set_enabled(pwm1_slice_num, true); // enable the PWM slice
-
-
-    gpio_init(MDAT);
-    gpio_init(MUX_A0);
-    gpio_init(MUX_A1);
-    gpio_init(MUX_A2);
-    gpio_init(COMP);
-    gpio_init(MCLK);
-
-    gpio_set_dir(MDAT, GPIO_OUT);
-    gpio_set_dir(MCLK, GPIO_OUT);
-    gpio_set_dir(MUX_A0, GPIO_OUT);
-    gpio_set_dir(MUX_A1, GPIO_OUT);
-    gpio_set_dir(MUX_A2, GPIO_OUT);
-    gpio_set_dir(COMP, GPIO_IN);
-    
-    gpio_put(MDAT, true);
-    sleep_us(10);
-    gpio_put(MCLK, true);
-    gpio_put(MUX_A0, true);
-    gpio_put(MUX_A1, true);
-    gpio_put(MUX_A2, true);
-
-
-    const uint led_pin = 25;
-
-    // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
-
-    // Initialize chosen serial port
-    stdio_init_all();
-
-    gpio_put(MUX_A0, false);
-    gpio_put(MUX_A1, false);
-    gpio_put(MUX_A2, false);
-
-
-
-    // Loop forever
+    // Start running our PIO program in the state machine
+    pio_sm_set_enabled(pio, sm, true);
+    // Do nothing
     while (true) {
-        gpio_put(led_pin, true);
-        sleep_ms(1000);
-        gpio_put(led_pin, false);
         sleep_ms(1000);
     }
+    //sm_config_set_jmp_pin();
 }
