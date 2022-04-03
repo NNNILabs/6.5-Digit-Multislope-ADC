c8dd6ed55ebf8a6ac4eb218dfc4cb4f54c25a1ab
diminDDL <dimin21590@gmail.com>
2022-04-03 20:28:01 +0200

    multislope all parts operational

--- src/Multislope/main.c
+++ src/Multislope/main.c
@@ -31,7 +31,8 @@ const uint8_t RESGP = 28;
 const uint8_t RESADC = 2;
 
 const uint64_t mainsPeriodus = 1000000/powerLineFreq;
-const uint32_t MScyclesPerPLC = multiSlopeFreq/powerLineFreq;
+//const uint32_t MScyclesPerPLC = multiSlopeFreq/powerLineFreq;
+const uint32_t MScyclesPerPLC = 6000;
 
 struct getCoutnsPayload
 {
@@ -44,18 +45,17 @@ struct getCoutnsPayload
 
 int64_t get_counts(PIO pio, uint sm , uint32_t countNum){
     uint32_t counts = 0;
-    uint32_t preRundownCounts = 0;
-    pio_sm_put_blocking(pio, sm, countNum);
+    uint32_t rundownCounts = 0;
+    pio_sm_put_blocking(pio, sm, countNum - 1);
     pio_sm_set_enabled(pio, sm, true);
     gpio_put(LED_PIN, true);
     counts = ~pio_sm_get_blocking(pio, sm);
-    //preRundownCounts = ~pio_sm_get_blocking(pio, sm);
+    rundownCounts = ~pio_sm_get_blocking(pio, sm);
     //printf("preRundownCounts: %u", preRundownCounts);
     gpio_put(LED_PIN, false);
     //pio_sm_set_enabled(pio, sm, false);
     return ((2*(int64_t)counts) - (int64_t)countNum);
-    //return (int64_t)counts;
-}
+    }
 
 bool measurement_timer_callback(struct repeating_timer *t) {
     void* payloadPtr = t->user_data;
@@ -102,7 +102,7 @@ int64_t get_counts_NPLC(PIO pio, uint sm , uint32_t NPLC){
 
 int main() {
     //set_sys_clock_khz(128000, true);                    // Set system clock to 128 Mhz to make the PIO clock frequency be evenly divisible by it
-    set_sys_clock_khz(100000, true);  
+    set_sys_clock_khz(96000, true);  
     stdio_init_all();
     /*static const*/ float pio_freq = multiSlopeFreq * 128;//32;  // freq * 32 cycles per count
 
@@ -137,7 +137,8 @@ int main() {
 
     // Calculate the PIO clock divider
     pio_freq = 10000000;
-    float div = (float)clock_get_hz(clk_sys) / pio_freq;
+    //float div = (float)clock_get_hz(clk_sys) / pio_freq;
+    float div = 10;
 
     // Initialize the program using the helper function in our .pio file
     ms_program_init(pio, multislopeSM, multislopeOffset, PWMA, COMP, div, MEAS);
@@ -154,7 +155,7 @@ int main() {
         // sleep_ms(500);
         // gpio_put(LED_PIN, false);
         // sleep_ms(500);
-        uint32_t reqCounts = 5000;
+        uint32_t reqCounts = 10;
 
         //configure PIO and start NPLC measurement
         //ms_program_init(pio, multislopeSM, multislopeOffset, PWMA, COMP, div, MEAS);
